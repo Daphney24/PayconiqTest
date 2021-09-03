42501237b862d1ed913a3aa7001ac96450f152a2
Daphney <dapaney24@gmail.com>
2021-09-03 20:46:53 +0200

    Added test for delete and updated the config

--- src/test/java/org/payconiq/test/BookingApiTest.java
+++ src/test/java/org/payconiq/test/BookingApiTest.java
@@ -2,6 +2,8 @@ package org.payconiq.test;
 
 import static io.restassured.RestAssured.given;
 
+import java.util.Properties;
+
 import org.payconiq.test.model.Booking;
 import org.payconiq.test.model.BookingDates;
 import org.testng.annotations.BeforeClass;
@@ -14,14 +16,20 @@ import io.restassured.specification.RequestSpecification;
 public class BookingApiTest {
 
 	private static RequestSpecification requestSpec;
-
+	private static Properties prop;
+	
+	private static final int SUCCESS_STATUS_CODE = 200;
+	private static final int CREATED_STATUS_CODE = 201;
+	
 	@BeforeClass
 	public void setup()  {
 		BaseTest baseTest = new BaseTest();
-		String url = baseTest.getProp().getProperty("api.baseUri");
+		prop = baseTest.getProp();
+		String url = prop.getProperty("api.baseUri");
+		String basePath = prop.getProperty("api.basePath");
 		requestSpec = new RequestSpecBuilder()
 				.setBaseUri(url)
-				.setBasePath("/booking")
+				.setBasePath(basePath)
 				.setContentType(ContentType.JSON)
 				.build();			
 	}
@@ -40,7 +48,7 @@ public class BookingApiTest {
 				post().
 		then().
 			assertThat().
-			statusCode(200);				
+			statusCode(SUCCESS_STATUS_CODE);				
 	}
 	
 	@Test
@@ -52,7 +60,23 @@ public class BookingApiTest {
 			    get("/1").
 	    then().
 			    assertThat().
-			    statusCode(200);
+			    statusCode(SUCCESS_STATUS_CODE);
+	}
+	
+	@Test
+	public void testDeleteEndpoint() {
+		
+		given().
+			  	spec(requestSpec).
+			  	auth().
+			  	preemptive()
+			  	.basic(prop.getProperty("username"),prop.getProperty("password")).
+	   and().
+	   		  delete("/25").
+	   then().
+	   		  assertThat().
+	   		  statusCode(CREATED_STATUS_CODE);
+	   
 	}
 	
 	@Test
--- src/test/java/org/payconiq/test/model/Booking.java
+++ src/test/java/org/payconiq/test/model/Booking.java
@@ -61,11 +61,5 @@ public class Booking {
 	public void setAdditionalneeds(String additionalneeds) {
 		this.additionalneeds = additionalneeds;
 	}
-	@Override
-	public String toString() {
-		return "Booking [firstname=" + firstname + ", lastname=" + lastname + ", totalprice=" + totalprice
-				+ ", depositpaid=" + depositpaid + ", bookingdates=" + bookingdates + ", additionalneeds="
-				+ additionalneeds + "]";
-	}
 	
 }
