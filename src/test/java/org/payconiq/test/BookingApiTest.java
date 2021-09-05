3bffa6969691bd20effc0ed564f709a1906aabb4
Daphney <dapaney24@gmail.com>
2021-09-05 21:51:05 +0200

    Implemented test for partial Booking and Delete

--- src/test/java/org/payconiq/test/BookingApiTest.java
+++ src/test/java/org/payconiq/test/BookingApiTest.java
@@ -2,21 +2,27 @@ package org.payconiq.test;
 
 import static io.restassured.RestAssured.given;
 
+import java.util.LinkedHashMap;
 import java.util.Properties;
 
+import org.json.simple.JSONObject;
 import org.payconiq.test.model.Booking;
 import org.payconiq.test.model.BookingDates;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class BookingApiTest {
 
 	private static RequestSpecification requestSpec;
 	private static Properties prop;
+	private Booking createdBooking;
+	private Integer createdBookingid = 28;
 	
 	private static final int SUCCESS_STATUS_CODE = 200;
 	private static final int CREATED_STATUS_CODE = 201;
@@ -34,59 +40,93 @@ public class BookingApiTest {
 				.build();			
 	}
 
-	@Test
+	@Test(priority = 1)
 	public void testCreateBookingEndpoint(){
 		  
 		BookingDates bookingDates = new BookingDates("2021-09-02", "2021-09-10");
-		Booking booking = new Booking("John", "Doe", 205, false, bookingDates, "Lunch");
+		createdBooking = new Booking("John", "Doe", 205, false, bookingDates, "Lunch");
 		
-		given().
+		createdBookingid = given().
 				spec(requestSpec).
 		and().
-				body(booking).
+				body(createdBooking).
 		when().
 				post().
 		then().
 			assertThat().
-			statusCode(SUCCESS_STATUS_CODE);				
+			statusCode(SUCCESS_STATUS_CODE).
+			extract().
+			path("bookingid");
+		
+		Assert.assertNotNull(createdBookingid, "Booking id not found");
+		System.out.println(createdBookingid);
 	}
 	
-	@Test
+	@Test(priority = 2)
 	public void testGetBookingByIdEndpoint() {
 		
 		given().
 				spec(requestSpec).
 		and().
-			    get("/1").
+			    get("/{bookingId}", createdBookingid).
 	    then().
 			    assertThat().
 			    statusCode(SUCCESS_STATUS_CODE);
 	}
-	
-	@Test
-	public void testDeleteEndpoint() {
+
+	@Test(priority = 3)
+	public void testGetBookingByIdResponse() {
 		
-		given().
-			  	spec(requestSpec).
-			  	auth().
-			  	preemptive()
-			  	.basic(prop.getProperty("username"),prop.getProperty("password")).
-	   and().
-	   		  delete("/25").
-	   then().
-	   		  assertThat().
-	   		  statusCode(CREATED_STATUS_CODE);
-	   
-	}
-	
-	@Test
-	public void testGetBookingIdsAndCheckParmaters() {
-		
-		given().
+		Booking booking =given().
 				spec(requestSpec).
 		and().
-	    	 	get("/1").
+			    get("/{bookingId}", createdBookingid).
 	    then().
-	    		extract();
+	    		contentType(ContentType.JSON).
+	    		extract().
+	    		as(Booking.class);
+		
+		Assert.assertEquals(booking.getFirstname(), createdBooking.getFirstname());			    
 	}
+	
+	@Test(priority =4)
+	public void testPartialUpdateBooking() {
+		
+		String newLastName = "dave";
+		JSONObject jsonRequestforUpdate = new JSONObject();
+		jsonRequestforUpdate.put("lastname", newLastName); 
+		
+		Booking updatedBooking =given().
+				spec(requestSpec).
+			  	auth().
+			  	preemptive().
+			  	basic(prop.getProperty("username"),prop.getProperty("password")).
+		and().
+				body(jsonRequestforUpdate).
+		when().	
+	    	 	patch("/{bookingId}", createdBookingid).
+	    then().
+	    		assertThat().
+	    		statusCode(SUCCESS_STATUS_CODE).
+	    		extract().
+	    		as(Booking.class);
+		
+		Assert.assertEquals(updatedBooking.getLastname(), newLastName);
+	}
+	
+	@Test(priority = 5)
+	public void testDeleteEndpoint() {
+		
+	   given().
+			  	spec(requestSpec).
+			  	auth().
+			  	preemptive().
+			  	basic(prop.getProperty("username"),prop.getProperty("password")).
+	   and().
+	   			delete("/{bookingId}", createdBookingid).
+	   then().
+	   		  	assertThat().
+	   		  	statusCode(CREATED_STATUS_CODE);
+	}
+
 }
